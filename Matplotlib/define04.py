afbb6c5c52f64b3d114046f150dd8fc13a2b30d2
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-08 16:18:55 +0900

    給与明細 使用関数

--- Matplotlib/define04.py
+++ Matplotlib/define04.py
@@ -1,9 +1,11 @@
 import datetime
 import csv
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-def InputData():
-    val = int(input())
+def InputData(msg):
+    val = int(input(msg))
 
     return val
 
@@ -30,6 +32,39 @@ def ReadCSV():
     file_name = "check_" + str(dt_now.year) + ".csv"
     csv_input = pd.read_csv(filepath_or_buffer = file_name, encoding = "utf_8", sep = ",")
     # インプットの項目数（行数 * カラム数）を返却します。
-    print(csv_input.size)
+    #print(csv_input.size)
     #返却される型は、numpy.ndarray
-    print(csv_input.values)
+    #print(csv_input.values)
+
+    return csv_input.values
+
+
+def checkMonth(csv_list):
+    length = len(csv_list)
+    this_month = csv_list[length-1][0]
+    dt_now = datetime.datetime.now()
+    if str(dt_now.month) + "月" == this_month:
+        return 1
+    else:
+        return 0
+
+
+def SortingData(csv_values):
+    month = []
+    data = []
+    for key, val in csv_values:
+        m = key.replace('月', '')
+        month.append(int(m))
+        data.append(val)
+    
+    return month, data
+
+
+def DrawingGraph(month, data):
+    
+    month = np.array(month)
+    data = np.array(data)
+    plt.plot(month, data)
+    plt.xlim([1, 13])
+    plt.ylim([0, 2000])
+    plt.show()
