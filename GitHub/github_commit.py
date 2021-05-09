5a4cb9f7186f96693d33598b257a00fe17f4928e
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-09 19:15:22 +0900

    GitHubのコミット数可視化プログラム

---
+++ GitHub/github_commit.py
@@ -0,0 +1,25 @@
+import os
+from def05 import *
+
+msg = "あなたのGitHubのユーザ名を入力してください >> "
+user = InputData(msg)
+url = "https://github.com/" + user
+# スクレイピングしコミット数に関連する部分をリスト化
+data_list = ScrapingGitHub(url)
+
+file_name = "sample.txt"
+# リスト化したデータでファイルを生成し、コミット数・日付のそれぞれをリスト化
+commit_list, date_list = MakeTxtInputList(data_list, file_name)
+# 不要なファイルを破棄
+os.remove(file_name)
+
+file_name = date_list[len(date_list)-1] + ".csv"
+# csvファイルを生成
+CheckFile(file_name)
+# データをcsvファイルに書き込み
+WrightCSV(file_name, commit_list, date_list)
+
+# csvを読み込みグラフを作成
+PrintGraph(file_name)
+# 不要なファイルを破棄
+os.remove(file_name)
