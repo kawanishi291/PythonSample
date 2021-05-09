14a9c4d69ee14ef4f8c790e49967c9d14918604a
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-05-09 20:26:15 +0900

    ログインのあるwebサイトのスクレイピング

---
+++ Sample/sample06.py
@@ -0,0 +1,13 @@
+from def06 import *
+
+# TOPページ表示関数
+# ScrapingTsudanumaInfo("http://localhost/html/index.html")
+
+# セッションを開始
+session = requests.session()
+# メールアドレスとパスワードの指定
+USER = "sneky56347.sk@gmail.com"
+PASS = "0000"
+# ログイン関数
+TsudanumaInfoLogin(USER, PASS)
+TsudanumaInfoMypage(session, "http://localhost/template/user.php")
